be688d30d3c42080ec0053ddde8743e8c3902795
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-07-20 21:43:32 +0200

    Fix mispelling

--- tests1.rb
+++ tests1.rb
@@ -1,14 +1,14 @@
 require_relative 'book'
 require_relative 'person'
-require_relative 'rentals'
+require_relative 'rental'
 
 person = Person.new(22, 'maximilianus')
 
 book1 = Book.new('title1', 'author1')
 book2 = Book.new('title2', 'author2')
 
-Rental.new(person, book1, '2022-01-01')
-Rental.new(person, book2, '2022-02-02')
+Rental.new('2022-01-01', person, book1)
+Rental.new('2022-02-02', person, book2)
 
 puts person.rentals
 puts book1.rentals
