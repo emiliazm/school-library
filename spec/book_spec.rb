0cc31aba0198ef841d2a8da7ebbb828930425b9a
unknown <skj99667788@gmail.com>
2022-07-27 19:46:20 +0500

    create Book test

---
+++ spec/book_spec.rb
@@ -0,0 +1,15 @@
+require_relative './../book'
+
+describe 'Book' do
+    context 'Create' do
+      it 'Book has the correct Info' do
+        title='potter'
+        author='saif'
+        book=Book.new(title, author)
+        book_title = book.title
+        book_author = book.author
+        expect(book_title).to eq(title)
+        expect(book_author).to eq(author)
+      end
+    end
+end
