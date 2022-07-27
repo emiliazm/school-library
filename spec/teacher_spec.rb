3a6e782c216083954bcc8c744cb8c54029cfa295
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-07-27 20:09:35 +0200

    Fix misspelling on teacher test

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -1,18 +1,18 @@
-require_relative '../teacher'
+require_relative './../teacher'
 
 describe Teacher do
-  context "When texting the Teacher class" do
-    teacher = Teacher.new(55, 'Math', 'Jhon', parent_permission: false, id: 1 )
-    
+  context 'When texting the Teacher class' do
+    teacher = Teacher.new(55, 'Math', 'Jhon', parent_permission: false, id: 1)
+
     teacher_specialization = teacher.specialization
     teacher_age = teacher.age
     teacher_name = teacher.name
-    can_use_services = teacher.can_use_services
+    can_use_services = teacher.can_use_services?
 
-    it "Create new teacher with given parameter" do
+    it 'Create new teacher with given parameter' do
       expect(teacher_age).to eql(55)
       expect(teacher_specialization).to eql('Math')
-      expect(teacher_name).to eql('John')
+      expect(teacher_name).to eql('Jhon')
       expect(can_use_services).to eql(true)
     end
   end
